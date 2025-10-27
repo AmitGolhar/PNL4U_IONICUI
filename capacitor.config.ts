93788eebe877598906d18c2141629dc8a8267be6
Amit Golhar <golharamit@gmail.com>
2025-10-27 07:11:39 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pnl4u',
+  webDir: 'www'
+};
+
+export default config;
